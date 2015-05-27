2582a3fd502fc4d24763173f171b4b6ec9153b9b
zach char <zcharnell@gmail.com>
2015-05-27 16:55:51 -0700

    photo uploader complete

--- server.js
+++ server.js
@@ -53,6 +53,12 @@ app.get("/listphotos", function (req, res) {
   });
 });
 
+
+
+
+
+
+
 app.use(methodOverride());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -65,6 +71,7 @@ app.use(errorHandler({
 
 var fs = require('fs');
 var AWS = require('aws-sdk');
+var aws = require('aws-sdk');
 var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
 var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
 var S3_BUCKET = process.env.S3_BUCKET;
@@ -91,15 +98,53 @@ app.post('/uploadFile', function(req, res){
                             
      fs.readFile(tmpPath, function (err, data) {
          var params = {
-             Bucket:'portfolio-unsplash',
-             ACL:'public-read',
-             Key:intname,
+             Bucket: 'portfolio-unsplash',
+             ACL: 'public-read',
+             Key: intname,
              Body: data
          };
          s3.putObject(params, function(err1, data) {});
      });
      
 });
+
+
+
+app.get('/sign_s3', function(req, res){
+    aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+    var s3 = new aws.S3();
+    console.log('envbucket: ' + process.env.S3_BUCKET);
+    //console.log('bucket ' +);
+    var s3_params = {
+        Bucket: 'portfolio-unsplash',
+        Key: req.query.file_name,
+        Expires: 60,
+        ContentType: req.query.file_type,
+        ACL: 'public-read'
+    };
+    s3.getSignedUrl('putObject', s3_params, function(err, data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            var return_data = {
+                signed_request: data,
+                url: 'https://'+ 'portfolio-unsplash' + '.s3.amazonaws.com/'+req.query.file_name
+            };
+            res.write(JSON.stringify(return_data));
+            res.end();
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
